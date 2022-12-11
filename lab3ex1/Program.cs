c30d1ce2d48ed133f497159b082b763c71a1872c
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2022-12-11 15:21:44 +0200

    Add project files.

---
+++ lab3ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab3ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 1
+                Scrieti o functie care va calcula suma cifrelor unui numar.
+             */
+
+            static int digitSum(int n)
+            {
+                int sum = 0;
+                while (n > 0)
+                {
+                    sum += n % 10;
+                    n = n / 10;
+                }
+                return sum;
+            }
+
+            Console.WriteLine("Introduceti un numar pentru a-i calcula suma cifrelor");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Suma cifrelor este {digitSum(n)}");
+        }
+    }
+}
